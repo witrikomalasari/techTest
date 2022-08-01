4adb164b02022985db6ccbf2f06bf79c812d75dd
witrikomalasari <dede.witri@gmail.com>
2022-08-01 09:39:36 +0700

    add listHarga from API w/ looping method

--- technicalTest/src/Pages/HargaUdang/index.js
+++ technicalTest/src/Pages/HargaUdang/index.js
@@ -30,28 +30,12 @@ const HargaUdang = props => {
   const {detailHarga} = useSelector(state => state.homeReducer);
 
   // console.log('resultDETAILHARGA', JSON.stringify(detailHarga, null, 2));
+
   const [listHarga, setListHarga] = useState([]);
 
   useEffect(() => {
     dispatch(getDetailHargaUdang());
-
-    // let harga = [];
-    // for (var keyObject in detailHarga) {
-    //   if (detailHarga.hasOwnProperty(keyObject)) {
-    //     let obj = keyObject + ' ' + detailHarga[keyObject];
-    //     harga.push(obj);
-    //     // console.log(key + " " + hargas[key]);
-    //   }
-    // }
-    // // let hargaUdang = [];
-    // for (let i = 10; i < harga.length; i + 10) {
-    //   if (harga[i].includes('size')) {
-    //     // hargaUdang.push(harga[i]);
-    //     setListHarga(harga[i]);
-    //   }
-    // }
-
-    // return () => price();
+    price(detailHarga);
   }, []);
 
   const {
@@ -63,54 +47,27 @@ const HargaUdang = props => {
   } = props.route.params.data;
 
   // PERCOBAAN CARI LIST HARGA, DATA FROM API
-  // const [listHarga, setListHarga] = useState([]);
-  // const price = () => {
-  //   let harga = [];
-  //   for (var keyObject in detailHarga) {
-  //     if (detailHarga.hasOwnProperty(keyObject)) {
-  //       let obj = keyObject + ' ' + detailHarga[keyObject];
-  //       harga.push(obj);
-  //       // console.log(key + " " + hargas[key]);
-  //     }
-  //   }
-  //   // let hargaUdang = [];
-  //   for (let i = 0; i < harga.length; i++) {
-  //     if (harga[i].includes('size')) {
-  //       // hargaUdang.push(harga[i]);
-  //       setListHarga(harga[i]);
-  //     }
-  //   }
-  // };
+  const price = detailHarga => {
+    let harga = [];
+    // let object;
+    for (var keyObject in detailHarga) {
+      if (detailHarga.hasOwnProperty(keyObject)) {
+        let obj = keyObject + ' ' + detailHarga[keyObject];
+        // console.log('ini coba jah', detailHarga(obj));
+        let object = obj;
+        // console.log('ini object', object);
+        harga.push(object.includes('size_') && object);
+        // harga.push(coba.filter(!Boolean));
+      }
+    }
+    let hargaUdang = harga.filter(
+      item =>
+        typeof item !== 'boolean' && item.replace(item.includes('remark'), ''),
+    );
+    setListHarga(hargaUdang);
+  };
 
   // console.log('ini list harga', listHarga);
-
-  // USESTATE HARDCODE KRN BELUM BERHASIL
-  // const [listHarga, setListHarga] = useState([
-  //   {id: 1, size: 'Size 20', price: 'Rp. 97.000'},
-  //   {id: 2, size: 'Size 21', price: 'Rp. 97.000'},
-  //   {id: 3, size: 'Size 22', price: 'Rp. 97.000'},
-  //   {id: 4, size: 'Size 23', price: 'Rp. 97.000'},
-  //   {id: 5, size: 'Size 24', price: 'Rp. 97.000'},
-  //   {id: 6, size: 'Size 25', price: 'Rp. 97.000'},
-  //   {id: 7, size: 'Size 26', price: 'Rp. 97.000'},
-  //   {id: 8, size: 'Size 27', price: 'Rp. 97.000'},
-  //   {id: 9, size: 'Size 28', price: 'Rp. 97.000'},
-  //   {id: 10, size: 'Size 29', price: 'Rp. 97.000'},
-  //   {id: 11, size: 'Size 30', price: 'Rp. 97.000'},
-  //   {id: 12, size: 'Size 31', price: 'Rp. 97.000'},
-  //   {id: 13, size: 'Size 32', price: 'Rp. 97.000'},
-  //   {id: 14, size: 'Size 33', price: 'Rp. 97.000'},
-  //   {id: 15, size: 'Size 34', price: 'Rp. 97.000'},
-  //   {id: 16, size: 'Size 35', price: 'Rp. 97.000'},
-  //   {id: 17, size: 'Size 36', price: 'Rp. 97.000'},
-  //   {id: 18, size: 'Size 37', price: 'Rp. 97.000'},
-  //   {id: 19, size: 'Size 38', price: 'Rp. 97.000'},
-  //   {id: 20, size: 'Size 39', price: 'Rp. 97.000'},
-  //   {id: 21, size: 'Size 40', price: 'Rp. 97.000'},
-  //   {id: 22, size: 'Size 41', price: 'Rp. 97.000'},
-  //   {id: 23, size: 'Size 42', price: 'Rp. 97.000'},
-  //   {id: 24, size: 'Size 43', price: 'Rp. 97.000'},
-  // ]);
 
   let hitung = listHarga.length;
 
@@ -182,24 +139,36 @@ const HargaUdang = props => {
           <Text style={styles.txt}>DAFTAR HARGA</Text>
           <Gap height={10} />
           <ScrollView>
-            <View style={{height: height * 0.7}}>
+            <View style={{height: height * 0.98}}>
               {listHarga.map(item => {
                 return (
-                  <View style={{flexDirection: 'row'}}>
-                    <Text style={styles.txt}>{item.size}</Text>
+                  <View
+                    key={item.id}
+                    style={{
+                      flexDirection: 'row',
+                      height: height / hitung,
+                      // backgroundColor: 'lightblue',
+                    }}>
+                    <Text style={styles.txt}>
+                      {item.substr(0, item.indexOf(' ')).replace('_', ' ')}
+                    </Text>
                     <Gap width={15} />
-                    <Text style={styles.txt}>{item.price}</Text>
+                    <Text style={styles.txt}>
+                      {`Rp. ${item.slice(item.search(' ', 0)).substring(1)}`}
+                    </Text>
                   </View>
                 );
               })}
             </View>
-            <Gap height={10} />
-            <Text style={styles.txt}>Catatan</Text>
-            <Text style={styles.txt}>
-              {remark
-                ? remark
-                : 'Lorem i psum dolor sit amet, consectetur adipiscing elit ut aliquam'}
-            </Text>
+            <Gap height={20} />
+            <View style={{paddingBottom: 20}}>
+              <Text style={styles.txt}>Catatan</Text>
+              <Text style={[styles.txt, {textAlign: 'justify'}]}>
+                {remark
+                  ? remark
+                  : 'Lorem i psum dolor sit amet, consectetur adipiscing elit ut aliquam'}
+              </Text>
+            </View>
           </ScrollView>
         </View>
       </View>
